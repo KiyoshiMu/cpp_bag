dbd54e3cf8dd61c80eb9ef483ed523b91425d930
KiyoshiMu <mooyewtsing@gmail.com>
2022-04-29 19:49:23 -0400

    cv 5

--- cpp_bag/plot.py
+++ cpp_bag/plot.py
@@ -163,7 +163,7 @@ def slide_vectors(
     projection = arr_project(val_embed)
     pred_probs: np.ndarray = knn.predict_proba(val_embed)
     val_labels = [simplify_label(l) for l in val["labels"]]
-    _df = proba_to_df(pred_probs, classes_, val_labels)
+    _df = proba_to_dfDict(pred_probs, classes_, val_labels)
     preds = knn.predict(val_embed)
     sizes = load_size()
     val_names = val["index"]
@@ -223,7 +223,16 @@ def slide_vectors(
     return fig
 
 
-def proba_to_df(pred_probs, classes_, val_labels):
+def cal_weighted_acc(label, *preds):
+    acc = 0
+    for rank, pred in enumerate(preds, start=1):
+        confident = float(pred.split(":")[-1])
+        acc += int(label in pred and "0.00" not in pred) * confident
+    return acc
+
+
+def proba_to_dfDict(pred_probs, classes_, val_labels):
+
     pred_probs_argsort = np.argsort(pred_probs, axis=1)[:, ::-1]
     prob_top0 = [
         f"{classes_[indices[0]]}:{pred_probs[row_idx, indices[0]]:.2f}"
@@ -245,12 +254,22 @@ def proba_to_df(pred_probs, classes_, val_labels):
         )
         for idx in range(len(val_labels))
     ]
+    weighted_acc = [
+        cal_weighted_acc(
+            val_labels[idx],
+            prob_top0[idx],
+            prob_top1[idx],
+            prob_top2[idx],
+        )
+        for idx in range(len(val_labels))
+    ]
     _df = {
         "label": val_labels,
         "prob_top0": prob_top0,
         "prob_top1": prob_top1,
         "prob_top2": prob_top2,
         "top3_correct": top3_corrects,
+        "weighted_acc": weighted_acc,
     }
     return _df
 
@@ -258,19 +277,23 @@ def proba_to_df(pred_probs, classes_, val_labels):
 def top3_summary(cases):
     correct_cases = cases[cases["top3_correct"]]
     incorrect_cases = cases[~cases["top3_correct"]]
+    weighted_acc_mean = cases["weighted_acc"].mean()
     summary = {
         "correct": (len(correct_cases), len(correct_cases) / len(cases)),
         "incorrect": (len(incorrect_cases), len(incorrect_cases) / len(cases)),
+        "weighted_acc": weighted_acc_mean,
     }
     return summary
 
 
 def dummy_exp(refer_embed, refer_labels, test_embed, test_labels):
-    dummy = DummyClassifier(strategy="stratified", random_state=42).fit(
+    dummy = DummyClassifier(strategy="prior", random_state=42).fit(
         refer_embed,
         refer_labels,
     )
     classes_ = dummy.classes_
     pred_probs = dummy.predict_proba(test_embed)
-    _df = proba_to_df(pred_probs, classes_, test_labels)
-    return top3_summary(pd.DataFrame(_df))
+    print(pred_probs[0])
+    _df = proba_to_dfDict(pred_probs, classes_, test_labels)
+    summary = top3_summary(pd.DataFrame(_df))
+    return summary
