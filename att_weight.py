b9642120a72794b25cf43af21da423e3b8a51047
KiyoshiMu <mooyewtsing@gmail.com>
2022-06-21 20:07:23 -0400

    plot att

--- att_weight.py
+++ att_weight.py
@@ -17,7 +17,31 @@ import plotly.colors
 
 TEMPLATE = "plotly_white"
 FONT = "Arial"
+CELL_TYPES: list[str] = [
+    "Neutrophil",
+    "Metamyelocyte",
+    "Myelocyte",
+    "Promyelocyte",
+    "Blast",
+    "Erythroblast",
+    "Megakaryocyte_nucleus",
+    "Lymphocyte",
+    "Monocyte",
+    "Plasma_cell",
+    "Eosinophil",
+    "Basophil",
+    "Histiocyte",
+    "Mast_cell",
+]
+def mk_color_map(cell_types):
+    colors = px.colors.qualitative.Alphabet
+    color_map = {t: colors[i % len(colors)] for i, t in enumerate(cell_types)}
+    return color_map
 
+COLOR_MAP = mk_color_map(CELL_TYPES)
+WIDTH = 940
+HEIGHT = 600
+WHITE_CANVAS = Image.new("RGB", (WIDTH, HEIGHT), color=(255, 255, 255))
 class AttentionWeightPlotter:
     def __init__(self, mp = "pool-1648142022566_MK.pth"):
         self.reducer = umap.UMAP()
@@ -32,31 +56,59 @@ class AttentionWeightPlotter:
         df.columns = ["D1", "D2"]
         df["cellType"] =  [cell.label for cell in cell_bag]
         attn_output_weights = self._attention_weight(feature_bag)[:len(cell_bag)]
-        df["weight"] = minmax_scale(attn_output_weights, (0, 1))
-        df["size"] = minmax_scale(attn_output_weights, (0.1, 1))
+        df["weight"] = minmax_scale(attn_output_weights, (0, 0.999))
+        df["size"] = minmax_scale(attn_output_weights, (0.1, 0.999))
         df["name"] = [cell.name for cell in cell_bag]
         return df
     
     def plot_on_df(self, df: pd.DataFrame, save_path: Path , img_loader: Callable[[str], Image.Image], marker=None ):
         fig = go.Figure()
-        fig.add_trace(
-                    go.Scatter(
-                        mode='markers',
-                        x=df["D1"],
-                        y=df["D2"],
-                        marker=dict(
-                            opacity=0,
-                        ),
-                        showlegend=False
-                    )
+        cell_types = df["cellType"].unique().tolist()
+        color_map = COLOR_MAP
+        x_min = df["D1"].min()
+        x_max = df["D1"].max()
+        y_min = df["D2"].min()
+        y_max = df["D2"].max()
+        for cell_type in cell_types:
+            cell_df = df[df["cellType"] == cell_type]
+            fig.add_trace(
+                go.Scatter(
+                    mode='markers',
+                    x=cell_df["D1"],
+                    y=cell_df["D2"],
+                    marker=dict(
+                        opacity=1,
+                        size=10,
+                        color=color_map[cell_type],
+                    ),
+                    showlegend=True,
+                    name=cell_type,
                 )
+            )
+
+        # hide dots
+        fig.add_layout_image(
+            dict(
+                source=WHITE_CANVAS,
+                xref="x",
+                yref="y",
+                x=x_min-1,
+                y=y_max+1,
+                sizex=(x_max-x_min+2),
+                sizey=(y_max-y_min+2),
+                sizing="stretch",
+                opacity=1,
+                layer="above")
+        )
+        
         slide_name = df.iloc[0]["name"].split(".")[0]
         for i, row in df.iterrows():
-            weight = row["weight"]
+            # weight = row["weight"]
             size = row["size"]
             cell_img =img_loader( f"{slide_name}/{row['name']}.jpg")
-            color = get_continuous_color(plotly.colors.PLOTLY_SCALES["RdBu"], weight)
-            cell_img = ImageOps.expand(cell_img,border=6,fill=color)
+            # color = get_continuous_color(plotly.colors.PLOTLY_SCALES["RdBu"], weight)
+            color = color_map[row["cellType"]]
+            cell_img = ImageOps.expand(cell_img,border=7,fill=color)
             fig.add_layout_image(
                 dict(
                     source=cell_img,
@@ -73,14 +125,14 @@ class AttentionWeightPlotter:
                     layer="above"
                 )
             )
-        fig.update_xaxes(showticklabels=False)
-        fig.update_yaxes(showticklabels=False)
+        fig.update_xaxes(showticklabels=False, showgrid=False)
+        fig.update_yaxes(showticklabels=False, showgrid=False)
         fig.update_layout(
             template=TEMPLATE,
             font_family=FONT,
-            legend=dict(
-                orientation="h",
-            ),
+            # legend=dict(
+            #     orientation="h",
+            # ),
             width=940,
             height=600,
             )
@@ -127,7 +179,7 @@ def main():
     for index in val_indices:
         feature, label, sample_cells = dataset.example_samples(index)
         df = att_plotter.make_att_df(sample_cells, feature)
-        att_plotter.plot_on_df(df, Path("data/att"), img_loader, marker=label)
+        att_plotter.plot_on_df(df, Path("data/att_"), img_loader, marker=label)
 
 
 def get_continuous_color(colorscale, intermed):
@@ -172,6 +224,19 @@ def get_continuous_color(colorscale, intermed):
         colortype="rgb")
     return tuple(int(float(f)) for f in re.findall(r'\d+.\d+', color_str))
 
+def add_pred_info(att_dir, ret_p):
+    with open(ret_p, "r") as f:
+        preds = json.load(f)
+    pred_map = {p["index"]: (p["label"], p["pred"]) for p in preds}
+    att_dir_map = {f.stem.split("-")[0]: f for f in Path(att_dir).glob("*.att.jpg")}
+    for k, v in att_dir_map.items():
+        label, pred = pred_map[k]
+        att_label = v.stem.split("-")[1]
+        assert label in att_label
+        v.rename(v.with_name(f"{k}-{label}-{pred}.att.jpg"))
+    
+
 
 if __name__ == "__main__":
-    main()
+    # main()
+    add_pred_info("data/att_", "slide_vectors.json")
